63f6aef3ac13ce4400f4224d82312635a1e4a597
chixonte <antonlavrentev405@gmail.com>
2025-04-08 20:11:53 +0000

    v 3.0

--- notepad.py
+++ notepad.py
@@ -16,19 +16,13 @@ def warning_save():
             return
         elif response:
             saveAs()
+            text.delete(1.0, END)
 
 #Функция создания нового файла
 def newFile():
     global filename
     filename = "Untitled"
-    t = text.get(1.0, END)
-    if t.strip():
-        response = messagebox.askyesnocancel("Notepad", "Хотите сохранить изменения?")
-        if response == None:
-            return
-        elif response:
-            saveAs()
-    text.delete(1.0, END)
+    warning_save()
     filename = None
     
     
@@ -36,17 +30,21 @@ def newFile():
 #Функция сохранения файла
 def saveFile():
     global filename
-    t = text.get(1.0, END)
-    with open(filename, "w") as f:
-        f.write(t)
-    
+    if not filename:
+        saveAs()
+        return
+    try:
+        with open(filename, "w") as f:
+            f.write(text.get(1.0, END))
+    except Exception as e:
+        messagebox.showerror("Error", f"Не удалось сохранить файл:\n{e}")
+
 #Функция сохранения как
 def saveAs():
     f = filedialog.asksaveasfile(mode='w', defaultextension='.txt')
     if f is None:
         return
     t = text.get(1.0, END)
-    
     try:
         f.write(t.rstrip())
     except:
@@ -63,13 +61,9 @@ def openFile():
     
 #Функция завершения программы
 def exit_programm():
-    if messagebox.askyesno("Text Editor", "Хотите сохранить изменения?"):
-        saveAs()
-        root.destroy()
-        root.quit()
-    else:
-        root.destroy()
-        root.quit()
+    warning_save() # cancel не работает
+    root.destroy()
+    root.quit()
 
 root = Tk()
 root.title("Text Editor by Chix ")
@@ -77,25 +71,57 @@ root.minsize(400, 400)
 root.maxsize(800, 600)
 
 #Панелька для нумерации
-numbers = Text(root, width=4, bg="#1e1e1e", state=DISABLED, relief=FLAT)
+numbers = Text(root, width=4, bg="#1e1e1e", fg="#d4d4d4", state=DISABLED, relief=FLAT)
 numbers.grid(row=0, column=0, sticky="NS")
+
+def on_yscrollcommand(*args):
+    scroll.set(*args)  # Синхронизация скролбара с текстовым полем
+    numbers.yview_moveto(args[0])  # Синхронизация поля с номерами с текстовым полем
+
+
 
 scroll = Scrollbar(root)
 scroll.grid(row=0, column=2, sticky="NS")
 
 
-text = Text(root, width=400, height=400, bg="#1e1e1e", fg="#d4d4d4", wrap=None)
+text = Text(root, width=400, height=400, yscrollcommand=on_yscrollcommand, bg="#1e1e1e", fg="#d4d4d4", wrap=None)
 text.grid(row=0, column=2, sticky="NSWE")
 
-#text.pack()
+def scroll_command(*args):
+    # Движение скролбара управляет отображением текста в обоих текстовых полях
+    text.yview(*args)
+    numbers.yview(*args)
+
+scroll.config(command=scroll_command)
+
+def insert_numbers():
+    count_of_lines = text.get(1.0, END).count('\n') + 1
+    
+    numbers.config(state=NORMAL)
+    numbers.delete(1.0, END)
+    numbers.insert(1.0, '\n'.join(map(str, range(1, count_of_lines))))
+    numbers.config(state=DISABLED)
+
+insert_numbers()
+
+def on_edit(event):
+    # Срабатывает при изменениях в текстовом поле
+    insert_numbers()
+    text.edit_modified(0)  # Сбрасываем флаг изменения текстового поля
+
+
+text.bind('<<Modified>>', on_edit)
+
+root.grid_columnconfigure(1, weight=1)
+root.grid_rowconfigure(0, weight=1)
 
 menubar = Menu(root, bg="#252526", fg="white")
 root.config(menu=menubar)
 
 #Меню FILE
 filemenu = Menu(menubar, tearoff=0, bg="#252526", fg="white")
-filemenu.add_command(label="New", command=newFile)
-filemenu.add_command(label="Open", command=openFile)
+filemenu.add_command(label="New", command=newFile, accelerator="Ctrl+N")
+filemenu.add_command(label="Open", command=openFile, accelerator="Ctrl+O")
 filemenu.add_command(label="Save", command=saveFile)
 filemenu.add_command(label="Save As...", command=saveAs)
 filemenu.add_separator()
@@ -115,6 +141,8 @@ menubar.add_cascade(label="Themes", menu=themesmenu)
 root.bind("<Control-s>", lambda e: saveAs())
 root.bind("<Control-o>", lambda e: openFile())
 root.bind("<Control-z>", lambda e: text.edit_undo())
+root.bind("<Control-n>", lambda e: newFile)
+
 
 
 root.protocol("WM_DELETE_WINDOW", exit_programm)
